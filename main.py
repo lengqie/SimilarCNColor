48d60a7bc502afbbf7647bd794a410bc4febf392
lengqie <lengqie@foxmail.com>
2022-01-09 22:11:13 +0800

    support hex

--- main.py
+++ main.py
@@ -1,25 +1,66 @@
 import json
+from enum import Enum
 
-in_color = input("please input the RGX or CMYk：")
-in_color = in_color.split(",")
-in_color = [int(i) for i in in_color]
 
-cmyk = False
+class InType(Enum):
+    RGB = "rgb"
+    CMYK = "cmyk"
+    HEX = "hex"
+
+
 res = []
+in_color = ""
+in_type = InType.RGB
 
-if len(in_color) == 4:
-    cmyk = True
+
+def InFilter(input_: str):
+    global in_type
+    global in_color
+    if "," in input_ and len(input_.split(",")) == 3:
+        in_type = InType.RGB
+        in_color = input_.split(",")
+        for i in in_color:
+            if not i.isdigit():
+                raise Exception("unfair input")
+        in_color = [int(i) for i in in_color]
+
+    elif "," in input_ and len(input_.split(",")) == 4:
+        in_type = InType.CMYK
+        in_color = input_.split(",")
+
+        for i in in_color:
+            if not i.isdigit():
+                raise Exception("unfair input")
+        in_color = [int(i) for i in in_color]
+
+    elif input_.startswith("#"):
+        in_type = InType.HEX
+        if len(input_) != 7:
+            raise Exception("unfair input")
+        in_color = input_[1:]
+        # hex -> rgb
+        in_color = [int(in_color[i:i + 2], 16) for i in range(0, len(in_color), 2)]
+        # print(in_color)
+
+    else:
+        raise Exception("unfair input")
 
 
-def GetColor(is_cmyk: bool):
+def GetColor(in_type_: InType):
+    code = ""
+    rng = 0
+
     with open("color.json", "r", encoding='utf-8') as file:
         Color = json.load(file)
 
     for i in Color:
-        if is_cmyk:
+        if in_type_ == InType.RGB:
+            code = "rgb"
+            rng = 3
+        elif in_type_ == InType.CMYK:
             code = "cmyk"
             rng = 4
-        else:
+        elif in_type_ == InType.HEX:
             code = "rgb"
             rng = 3
         code = i[code].split("/")
@@ -33,6 +74,8 @@ def GetColor(is_cmyk: bool):
             }
         )
 
+    res.sort(key=DictSort)
+
 
 def DictSort(ele: dict) -> int:
     return ele["val"]
@@ -45,6 +88,7 @@ def Head(li: list, nb: int = 5):
 
 
 if __name__ == '__main__':
-    GetColor(cmyk)
-    res.sort(key=DictSort)
+    in_color = input("please input the RGX or CMYk or HEX：")
+    InFilter(in_color)
+    GetColor(in_type)
     Head(res, 5)
